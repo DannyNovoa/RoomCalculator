dc65576dfcd9e00f67900210c2e0c0e4586452a8
RenoNovoa <djnova25@gmail.com>
2021-01-15 16:50:55 -0500

    Window Store

--- Program.cs
+++ Program.cs
@@ -6,8 +6,58 @@ namespace RoomCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("we are making new program ");
+            string answer;
+            do
+            {
+
+            
+            Console.WriteLine(" Welcome to the Grand Circus window store!\n ");
+            Console.WriteLine("Let's first Calculate the material you are going to  need for your window.\n ");
+            Console.Write("Please enter your name here : ");
+            string clientOftheStore = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Please enter the (Length) of your windows: ");
+            double windowlength = double.Parse(Console.ReadLine());
+
+            Console.Write("Now please enter the (Width)of your windows: ");
+            double windowWidth = double.Parse(Console.ReadLine());
+
+
+            double areaOfWindow = windowlength * windowWidth;
+            double perimeterOfWindow = (2 *(windowlength + windowWidth));
+            Console.WriteLine();
+            Console.WriteLine($"Hola amigo {clientOftheStore}. For this job you will need\n" +
+                $"{areaOfWindow} square centimeters of glass for your window. ");
+            Console.WriteLine();
+            Console.WriteLine($" And for this window you will need\n" +
+               $"{perimeterOfWindow} centimeters of wood to make the frame  ");
+                Console.WriteLine();
+                bool isInvalid;
+                do
+                {
+                    Console.Write("Would you like to continue (y/n) ?");
+                    answer = Console.ReadLine();
+                    isInvalid = answer != "y" && answer != "n";
+                    if (isInvalid)
+                    {
+                        Console.WriteLine("OOPS! Your input is incorrect try again please ");
+                    }
+                    else if (answer == "n")
+                    {
+                        Console.WriteLine("Adios Amigo! ");
+                    }
+                   
+                    //Console.WriteLine();
+
+                } while (isInvalid);
+               // Console.WriteLine("Adios Amigo! ");
+
+                
+            } while (answer == "y");
+
+
+            Console.ReadKey();
         }
     }
 }
